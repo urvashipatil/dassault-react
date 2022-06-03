50e493ade093f80ca459254d39eeb6ab4e79856e
Urvashi Amit Sachdev <urvashipatil@gmail.com>
2022-06-03 15:53:54 +0530

    added test case for counter intesting folder

--- src/components/search.js
+++ src/components/search.js
@@ -19,30 +19,30 @@ export default function Search(props) {
   let debounceValue = useDebounce(state, 1000);
   const throwError = useAsyncError();
 
-  useEffect(() => {
-    // throw new Error("Some error");
-    fetch("http://some-site.wtf/rest11")
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-      })
-      .catch((e) => {
-        console.log("Error");
-        // throw new Error("Asynchronous error");
-        throwError("Asynchronous error");
-      });
-  }, []);
-
   // useEffect(() => {
-  //   //api call
-  //   async function fetchSugestions() {
-  //     let response = await axios.get(
-  //       "https://jsonplaceholder.typicode.com/posts?_limit=10"
-  //     );
-  //     console.log("axios response");
-  //   }
-  //   fetchSugestions();
-  // }, [debounceValue]);
+  //   // throw new Error("Some error");
+  //   fetch("http://some-site.wtf/rest11")
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       console.log(data);
+  //     })
+  //     .catch((e) => {
+  //       console.log("Error");
+  //       // throw new Error("Asynchronous error");
+  //       throwError("Asynchronous error");
+  //     });
+  // }, []);
+
+  useEffect(() => {
+    //api call
+    async function fetchSugestions() {
+      let response = await axios.get(
+        "https://jsonplaceholder.typicode.com/posts?_limit=10"
+      );
+      console.log("axios response");
+    }
+    fetchSugestions();
+  }, [debounceValue]);
 
   const onHandleChange = async (e) => {
     setState(e.target.value);
